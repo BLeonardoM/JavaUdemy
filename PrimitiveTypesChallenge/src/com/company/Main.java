dea1baffc5ac6f0b4979d05f6d7ac5c078b2f4b2
Bruno <79713149+BLeonardoM@users.noreply.github.com>
2021-08-07 15:38:42 -0300

    Add files via upload

---
+++ PrimitiveTypesChallenge/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //Chalenge
+        byte myByteNumber = 1;
+        short myShortNumber = 2;
+        int myIntNumber = 3;
+
+        long  totalNumber = ((myByteNumber + myShortNumber + myIntNumber) * 10) + 50000;
+        System.out.println("Challenge number = " + totalNumber);
+    }
+}
